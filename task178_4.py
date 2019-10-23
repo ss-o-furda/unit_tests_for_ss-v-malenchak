253d4204258dd3170a333e817e6088ed8d5a2d6c
Orest <orik@orikpc.ddns.softservecom.com>
2019-10-23 15:05:02 +0300

    some change

--- task178_4.py
+++ task178_4.py
@@ -1,8 +1,16 @@
-def get_elements_less_arithmetic_mean_of_its_neighbor(sequence):
+"""
+178.4
+Даны натуральные числа n, A1, ..., An.
+Определить количество членов Ak последовательности A1, ..., An
+удовлетворящих условию Ak < (Ak-1 + Ak+1) / 2.
+"""
+__author__ = 'Valentyn Malenchak'
+
+def get_count_of_elements_less_than_arithmetic_mean_of_its_neighbor(sequence):
     """
     Count how many integer numbers satisfy the condition
-    Ak < (Ak-1 + Ak+1 ) / 2.
-    :param sequence: some sequence of integer number.
+    Ak < (Ak-1 + Ak+1) / 2.
+    :param sequence: some sequence of integer numbers.
     :return: count of numbers.
     """
     arithmetic_means_for_elements = {sequence[i]:
@@ -11,3 +19,19 @@ def get_elements_less_arithmetic_mean_of_its_neighbor(sequence):
     filtered_values = filter(lambda element: element[0] < element[1],
                              arithmetic_means_for_elements.items())
     return len(dict(filtered_values))
+
+
+def task_178_4_menu():
+    print('-' * 100)
+    print(f'Function logic:\n{get_count_of_elements_less_than_arithmetic_mean_of_its_neighbor.__doc__}')
+    print('-' * 50)
+    print("Input some sequence of integer numbers by \' \':\n")
+    try:
+        sequence = [int(number) for number in input().split(' ')]
+        print(f'Result: {get_count_of_elements_less_than_arithmetic_mean_of_its_neighbor(sequence)}')
+    except Exception as error:
+        print(f'{error}. Try again!'.title())
+
+
+if __name__ == '__main__':
+    task_178_4_menu()
